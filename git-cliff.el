a75fcaa17e2983e3abce406da7415f9c4075378f
liuyinz <liuyinz95@gmail.com>
2023-08-29 23:07:54 +0800

    chore(init): initial commit

---
+++ git-cliff.el
@@ -0,0 +1,40 @@
+;;; git-cliff.el --- Generate changelog based on git-cliff -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 liuyinz
+
+;; Author: liuyinz <liuyinz95@gmail.com>
+;; Maintainer: liuyinz <liuyinz95@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.3"))
+;; Keywords: tools
+;; Homepage: https://github.com/liuyinz/git-cliff
+
+;; This file is not a part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; This file is not a part of GNU Emacs.
+
+;;; Commentary:
+
+;; Generate and update CHANGELOG file in Emacs.
+;; This package provides the interface `git-cliff-menu' which is based on git-cliff,
+;; to generate changelog for project.  Call `conventional-changelog-menu' to start.
+
+;;; Code:
+
+
+
+(provide 'git-cliff)
+;;; git-cliff.el ends here
